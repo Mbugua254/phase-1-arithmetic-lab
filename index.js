63f91e8299a1e9978442971260ec871868a2b06c
Michael Mbugua <mikembugua254@gmail.com>
2024-04-04 09:38:14 +0300

    Got the remainder, maximum value, multiplication and random numbers

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+//multiplies
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+//Generates random numbers greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+//Gets the remainder
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+//Gets the maximum value
+let numbers = [2, 8, 15, 20, 1, 7];
+let max = Math.max(...numbers);
+
+
